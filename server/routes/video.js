494aa1868d8875fd3a4b47a4b00aa18aefc7b3c1
tudy <jhh0207@hanmail.net>
2021-08-09 23:59:50 +0900

    ffmpeg로 비디오 썸네일 생성하기(ing)

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
+const ffmpeg = require("fluent-ffmpeg");
 
 
 // STORAGE MULTER CONFIG
@@ -41,5 +42,44 @@ router.post("/uploadfiles", (req, res) => {
 
 });
 
+
+router.post("thumbnail", (req, res) => {
+
+    let filePath = "";
+    let fileDuration = "";
+    
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function (err, metadata){
+        console.log(metadata);
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration;
+    })
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+        console.log('Will generate ' + filenames.join(', '))
+        console.log(filenames)
+
+        filePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end', function() {
+        console.log('Screenshots taken');
+        return res.json({ success: true, url: filePath, fileName : filenames, fileDuration : fileDuration});
+    })
+    .on('error', function (err) {
+        console.error(err);
+        return res.json({success: false, err});
+    })
+    .screenshot({
+        count: 3, 
+        folder: 'uploads/thumbnails',
+        size : '320x240',
+        filename : 'thumbnail-%b.png'
+    })
+
+
+});
 
 module.exports = router;
